2cf58ef45c6913295e6e93d0baaac7617efdcbc6
Kieron25 <78971973+Kieron25@users.noreply.github.com>
2023-01-19 14:49:06 +0000

    Add files via upload
    Version 2 of this file.

--- eigenvalue level repulsion.jl
+++ eigenvalue level repulsion.jl
@@ -5,6 +5,7 @@ using Arpack
 using Plots
 #using Random
 using SparseArrays
+using Statistics
 
 #include("diagsparse.jl")
 
@@ -40,18 +41,44 @@ function Hrand(D, ρ)
     return H
 end
 
-D = 25; ρ = 0.5; neigv = D-10 #λ = 0.1;
-A = Hrand(D, ρ); B = Hrand(D, ρ)
-EV = []; X = 0.0:0.05:1
+D = 400; 
+ρ = 0.8; neigv = D-1 #λ = 0.1;
+A = Hrand(D, ρ); B = Hrand(D, ρ); λ = 0.7
+#=EV = []; X = 0.0:0.1:1
 for λ in X
     Aλ = λ * A; Bλ = (1 - λ) * B
     Mλ = Aλ + Bλ
     evλ = eigs(Mλ, nev = neigv, which=:LR)[1]
     push!(EV, evλ)
-end
+end=#
 #display(EV)
+Aλ = λ * A; Bλ = (1 - λ) * B
+Mλ = Aλ + Bλ
+evλ = eigs(Mλ, nev = neigv, which=:SR)[1] # SR is given since we want eigenvalues in ascending order so pick the smallest ones first
+#display(evλ)
 
+Δ = []
+for n in 1:lastindex(evλ)-1
+    #δ_nminus1 = evλ[n] - evλ[n-1]
+    δ_n = evλ[n+1] - evλ[n]
+    #r_n = min(δ_n, δ_nminus1)/max(δ_n, δ_nminus1)
+    append!(Δ, δ_n)
+    #append!(Δ, r_n)
+end
+meanΔ = mean(Δ); l = 84
+#display(Δ)
+bins = range(1e-4, 5, length=l)
+graph = histogram(Δ/meanΔ, bins=bins, normalize=:pdf, label="Data from $D x $D \nrandom matrix")
+xlabel!("s / difference in eigenvalues ÷ average difference")
+Y1 = []
+for s in bins
+    y = (pi/2) * s * exp( -(pi * s^ 2)/4)
+    append!(Y1, y)
+end
+plot!(bins, Y1, label="Probability Distribution for \nGaussian Orthogonal Ensemble")
+display(graph)
 #l = D-1
+#=
 Y = []
 for i in 1:lastindex(X)
     append!(Y, EV[i][1])
@@ -70,6 +97,7 @@ for i in 2:neigv
     plot!(X, Yj, label="eigenvalue $i")
 end
 display(graph1)
+=#
 
 #=
 graph = scatter(X, Y, title=titlestr, label=label1, legendposition=:bottomright, ylims = (y2-0.15, y2+0.1)) # yerror = ESeb,
